ca0d4e747a4082b1033b1f1124fe066ab6c55195
Uexcel2022 <excellenceudoka@gmail.com>
2023-11-23 21:39:45 +0100

    commit

--- src/main/java/com/uexcel/oauth/client/OauthClientApplication.java
+++ src/main/java/com/uexcel/oauth/client/OauthClientApplication.java
@@ -2,12 +2,33 @@ package com.uexcel.oauth.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OauthClientApplication {
+
+	@Bean
+	RouteLocator gateWay(RouteLocatorBuilder rlb) {
+		return rlb
+				.routes()
+				.route(rs -> rs
+						.path("/home")
+						.filters(GatewayFilterSpec::tokenRelay)
+						.uri("http://localhost:9000"))
+				.build();
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthClientApplication.class, args);
 	}
+
+
 
 }
